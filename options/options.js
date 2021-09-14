37f6bd712106d503eb453f383e328792222f0513
Crowdbotics <team@crowdbotics.com>
2021-09-14 17:21:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_haze_30539Identifier",
+  url: "https://frosty_haze_30539Identifier.botics.co",
+  api: "https://frosty_haze_30539Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
